c890ffe1583d65259f75afb5c88f2f8ddeabdf7c
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-11-08 21:58:57 +0530

    Refactored code  to get working hours using functions

--- EmployeWage.sh
+++ EmployeWage.sh
@@ -1,17 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 isPartTime=1
 isFullTime=2
 maxHrsInMonth=10
 empRatePerHr=20
 numWorkingDays=20
+
 totalEmpHrs=0
 totalWorkingDays=0
 
-while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
-do
-	(($totalWorkingDays++))
-	empCheck=$(( RANDOM%3 ))
-	case $empCheck in
+function getWorkingHours(){
+	case $1 in
 		$isPartTime)
 			empHrs=4
 			;;
@@ -22,8 +20,12 @@ do
 			empHrs=0
 			;;
 	esac
+	echo $empHrs
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+	totalWorkingDays=$(($totalWorkingDays+1))
+	empHrs="$(getWorkingHours $(( RANDOM%3 )) )"
 	totalEmpHrs=$(($totalEmpHrs+$empHrs))
-done
 	totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
-
-
+done
