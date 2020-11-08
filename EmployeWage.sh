dc8aebbb8f29d311a7568070dcdec382d7c1793b
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-11-08 22:20:12 +0530

    Stored day and dailywage along with total wage

--- EmployeWage.sh
+++ EmployeWage.sh
@@ -1,14 +1,13 @@
-#!/bin/bash -x 
 isPartTime=1
 isFullTime=2
 maxHrsInMonth=10
 empRatePerHr=20
 numWorkingDays=20
 
-totalEnpHrs=0
+totalEmpHrs=0
 totalWorkingDays=0
 
-function getWorkingHours() {
+function getWorkingHours(){
 	case $1 in
 		$isPartTime)
 			empHrs=4
@@ -22,18 +21,19 @@ function getWorkingHours() {
 	esac
 	echo $empHrs
 }
-function calDailyWages() {
+
+function calDailyWages(){
 	totalWorkHrs=$1;
 	wages=$(( $totalWorkHrs*$empRatePerHr ))
 	echo $wages;
 }
-while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+while [[ $totalEmpHrs -lt $maxHrsMonth && $totalWorkingDays -lt $numWorkingDays ]]
 do
 	totalWorkingDays=$(($totalWorkingDays+1))
-	empHrs="$( getWorkingHrs $((RANDOM%3)) )"
-	totalEmpHrs=$(( $totalEmpHrs+$empHrs ))
-	empDailyWages[$totalWorkingDays]="$(calDailyWages$empHrs)"
+	empHrs="$(getWorkingHours $(( RANDOM%3)) )"
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	empDailyWages[$totalWorkingDays]="$(calDailyWages $empHrs)"
 done
-totalSalary="$(calDailyWages*$totalEmpHrs)"
-echo "daily wages :"
-    ${empDailyWages[@]}
+totalSalary="$(calDailyWages $totalEmpHrs)"
+echo "daily wages :"${empDailyWages[@]}
+echo "All Keys :"${!empDailyWages[@]}
